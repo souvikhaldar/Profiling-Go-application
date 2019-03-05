1a36c69c9a90c39b87451712cb05f1b631a760d2
Souvik Haldar <souvik.haldar@kartbites.com>
2019-03-05 10:32:18 +0530

    CPU and memory profiling done

---
+++ prime.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/profile"
+)
+
+func prime(lim int) []int {
+	primes := make([]int, 0)
+	for i := 1; i < lim; i++ {
+		flag := 0
+		for j := 2; j < i; j++ {
+			if i%j == 0 {
+				flag = 1
+				break
+			}
+		}
+		if flag == 0 {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+func main() {
+	defer profile.Start().Stop()
+	x := prime(100000)
+	fmt.Println(x)
+}
